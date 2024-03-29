b62527b28fe71a7398eaf88ed9c791f3f98f4758
Atul Jha <43900716+atuljha062@users.noreply.github.com>
2024-03-29 14:12:10 +0530

    Update 2. Data Loading.sql

--- SQL/2. Data Loading.sql
+++ SQL/2. Data Loading.sql
@@ -1,29 +1,49 @@
-// Creating external stage (create your own bucket)
-CREATE OR REPLACE STAGE SCD_DEMO.SCD2.customer_ext_stage
-    url='s3://dw-snowflake-course-darshil/stream_data'
-    credentials=(aws_key_id='' aws_secret_key='');
-   
+-- Ingesting Data in the tables
 
-CREATE OR REPLACE FILE FORMAT SCD_DEMO.SCD2.CSV
-TYPE = CSV,
-FIELD_DELIMITER = ","
-SKIP_HEADER = 1;
+USE DATABASE SCD_DEMO;
+
+USE SCHEMA SCD_DEMO.SCD2
+
+
+-- Creting Storage Integration
+CREATE OR REPLACE STORAGE INTEGRATION s3_init_real_time
+TYPE = EXTERNAL_STAGE
+STORAGE_PROVIDER = S3
+ENABLED = TRUE
+STORAGE_AWS_ROLE_ARN = 'arn:aws:iam::905418044599:role/snowflake-real-time-project-s3-connection'
+STORAGE_ALLOWED_LOCATIONS = ('s3://dw-snowflake-atul');
+
+DESC STORAGE INTEGRATION s3_init_real_time;
+
+-- Creating Stage
+CREATE OR REPLACE STAGE customer_ext_stage
+url = 's3://dw-snowflake-atul/stream_data'
+STORAGE_INTEGRATION = s3_init_real_time;
 
 SHOW STAGES;
 LIST @customer_ext_stage;
 
+-- Creating File Format
+CREATE OR REPLACE FILE FORMAT csv
+TYPE = CSV
+FIELD_DELIMITER = ','
+SKIP_HEADER = 1;
 
+-- Creating Snowpipe
 CREATE OR REPLACE PIPE customer_s3_pipe
-  auto_ingest = true
-  AS
-  COPY INTO customer_raw
-  FROM @customer_ext_stage
-  FILE_FORMAT = CSV
-  ;
+AUTO_INGEST = TRUE
+AS
+COPY INTO CUSTOMER_RAW
+FROM @customer_ext_stage
+FILE_FORMAT = CSV;
 
-show pipes;
-select SYSTEM$PIPE_STATUS('customer_s3_pipe');
+-- Check pipes details to get the notification channel
+SHOW PIPES;
 
-SELECT count(*) FROM customer_raw limit 10;
+-- Check current status of pipe
+SELECT SYSTEM$PIPE_STATUS('customer_s3_pipe');
 
-TRUNCATE  customer_raw;
+
+SELECT COUNT(*) FROM customer_raw;
+
+SELECT * FROM customer_raw;
