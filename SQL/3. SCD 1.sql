268b0e7862b24aa2c04f71fb63718d8536afd4e5
Atul Jha <43900716+atuljha062@users.noreply.github.com>
2024-03-29 14:14:50 +0530

    Update 3. SCD 1.sql

--- SQL/3. SCD 1.sql
+++ SQL/3. SCD 1.sql
@@ -1,90 +1,110 @@
-merge into customer c 
-using customer_raw cr
-   on  c.customer_id = cr.customer_id
-when matched and c.customer_id <> cr.customer_id or
-                 c.first_name  <> cr.first_name  or
-                 c.last_name   <> cr.last_name   or
-                 c.email       <> cr.email       or
-                 c.street      <> cr.street      or
-                 c.city        <> cr.city        or
-                 c.state       <> cr.state       or
-                 c.country     <> cr.country then update
-    set c.customer_id = cr.customer_id
-        ,c.first_name  = cr.first_name 
-        ,c.last_name   = cr.last_name  
-        ,c.email       = cr.email      
-        ,c.street      = cr.street     
-        ,c.city        = cr.city       
-        ,c.state       = cr.state      
-        ,c.country     = cr.country  
-        ,update_timestamp = current_timestamp()
-when not matched then insert
-           (c.customer_id,c.first_name,c.last_name,c.email,c.street,c.city,c.state,c.country)
-    values (cr.customer_id,cr.first_name,cr.last_name,cr.email,cr.street,cr.city,cr.state,cr.country);
-    
+USE DATABASE SCD_DEMO;
+
+USE SCHEMA SCD_DEMO.SCD2;
+
+-- Merge statement to insert data into the actual table based on condition
+-- MERGE INTO customer AS c
+-- USING customer_raw AS cr
+--     ON c.customer_id = cr.customer_id
+-- WHEN matched and c.customer_id <> cr.customer_id OR
+--                  c.first_name  <> cr.first_name  OR
+--                  c.last_name   <> cr.last_name   OR
+--                  c.email       <> cr.email       OR
+--                  c.street      <> cr.street      OR
+--                  c.city        <> cr.city        OR
+--                  c.state       <> cr.state       OR
+--                  c.country     <> cr.country then update
+--      set c.customer_id = cr.customer_id ,
+--          c.first_name  = cr.first_name  ,
+--          c.last_name   = cr.last_name   ,
+--          c.email       = cr.email       ,
+--          c.street      = cr.street      ,
+--          c.city        = cr.city        ,
+--          c.state       = cr.state       ,
+--          c.country     = cr.country,
+--          update_timestamp = current_timestamp()
+-- WHEN not matched then insert
+--            (c.customer_id,c.first_name,c.last_name,c.email,c.street,c.city,c.state,c.country)
+--      VALUES(cr.customer_id,cr.first_name,cr.last_name,cr.email,cr.street,cr.city,cr.state,cr.country);
 
 
-CREATE OR REPLACE PROCEDURE pdr_scd_demo()
-returns string not null
+-- Instead of running the query again and again manually we will put ti inside Stored Procedure
+CREATE OR REPLACE PROCEDURE pdr_scd_demo() 
+RETURNS string not null
 language javascript
-as
+AS
     $$
-      var cmd = `
-                 merge into customer c 
-                 using customer_raw cr
-                    on  c.customer_id = cr.customer_id
-                 when matched and c.customer_id <> cr.customer_id or
-                                  c.first_name  <> cr.first_name  or
-                                  c.last_name   <> cr.last_name   or
-                                  c.email       <> cr.email       or
-                                  c.street      <> cr.street      or
-                                  c.city        <> cr.city        or
-                                  c.state       <> cr.state       or
-                                  c.country     <> cr.country then update
-                     set c.customer_id = cr.customer_id
-                         ,c.first_name  = cr.first_name 
-                         ,c.last_name   = cr.last_name  
-                         ,c.email       = cr.email      
-                         ,c.street      = cr.street     
-                         ,c.city        = cr.city       
-                         ,c.state       = cr.state      
-                         ,c.country     = cr.country  
-                         ,update_timestamp = current_timestamp()
-                 when not matched then insert
-                            (c.customer_id,c.first_name,c.last_name,c.email,c.street,c.city,c.state,c.country)
-                     values (cr.customer_id,cr.first_name,cr.last_name,cr.email,cr.street,cr.city,cr.state,cr.country);
-      `
-      var cmd1 = "truncate table SCD_DEMO.SCD2.customer_raw;"
-      var sql = snowflake.createStatement({sqlText: cmd});
-      var sql1 = snowflake.createStatement({sqlText: cmd1});
-      var result = sql.execute();
-      var result1 = sql1.execute();
-    return cmd+'\n'+cmd1;
+        var cmd = `
+            MERGE INTO customer AS c
+            USING customer_raw AS cr
+                ON c.customer_id = cr.customer_id
+            WHEN matched and c.customer_id <> cr.customer_id OR
+                             c.first_name  <> cr.first_name  OR
+                             c.last_name   <> cr.last_name   OR
+                             c.email       <> cr.email       OR
+                             c.street      <> cr.street      OR
+                             c.city        <> cr.city        OR
+                             c.state       <> cr.state       OR
+                             c.country     <> cr.country then update
+                 set c.customer_id = cr.customer_id ,
+                     c.first_name  = cr.first_name  ,
+                     c.last_name   = cr.last_name   ,
+                     c.email       = cr.email       ,
+                     c.street      = cr.street      ,
+                     c.city        = cr.city        ,
+                     c.state       = cr.state       ,
+                     c.country     = cr.country,
+                     update_timestamp = current_timestamp()
+            WHEN not matched then insert
+                       (c.customer_id,c.first_name,c.last_name,c.email,c.street,c.city,c.state,c.country)
+                 VALUES(cr.customer_id,cr.first_name,cr.last_name,cr.email,cr.street,cr.city,cr.state,cr.country);
+        `
+        
+        var cmd1 = 'TRUNCATE TABLE SCD_DEMO.SCD2.customer_raw;'
+        
+        var sql = snowflake.createStatement({sqlText:cmd});
+        var sql1 = snowflake.createStatement({sqlText:cmd1});
+
+        var result = sql.execute();
+        var result1 = sql1.execute();
+
+      return cmd+'\n'+cmd1;
+        
     $$;
+
+-- manually calling stored procedure
 call pdr_scd_demo();
 
+-- To automate the above step we create Task
+-- Before creating Task we need to create Users and permissions
 
---Set up TASKADMIN role
-use role securityadmin;
-create or replace role taskadmin;
--- Set the active role to ACCOUNTADMIN before granting the EXECUTE TASK privilege to TASKADMIN
-use role accountadmin;
-grant execute task on account to role taskadmin;
+-- Set up TASKADMIN role
+USE ROLE securityadmin;
+CREATE OR REPLACE ROLE taskadmin;
 
--- Set the active role to SECURITYADMIN to show that this role can grant a role to another role 
-use role securityadmin;
-grant role taskadmin to role sysadmin;
+-- Set the active role to ACCOUNTADMIN before granting the EXECUTE TASK privilages to TASKADMIN
+USE ROLE ACCOUNTADMIN;
+GRANT EXECUTE TASK ON ACCOUNT to ROLE TASKADMIN;
 
-create or replace task tsk_scd_raw warehouse = COMPUTE_WH schedule = '1 minute'
-ERROR_ON_NONDETERMINISTIC_MERGE=FALSE
+-- Set the active role to SECURITYADMIN to show that this role can grant a role to another role
+USE ROLE SECURITYADMIN;
+GRANT ROLE taskadmin to ROLE sysadmin;
+
+-- Creating Task
+CREATE OR REPLACE TASK tsk_scd_raw warehouse = COMPUTE_WH schedule = '1 minute'
+ERROR_ON_NONDETERMINISTIC_MERGE = FALSE
 as
 call pdr_scd_demo();
-show tasks;
-alter task tsk_scd_raw suspend;--resume --suspend
-show tasks;
 
-select timestampdiff(second, current_timestamp, scheduled_time) as next_run, scheduled_time, current_timestamp, name, state 
-from table(information_schema.task_history()) where state = 'SCHEDULED' order by completed_time desc;
+SHOW TASKS;
+
+-- After creating the Task the state of the Task will be suspended, we need to resume it
+
+ALTER TASK tsk_scd_raw resume;
 
 
-select * from customer where customer_id=0;
+SELECT * FROM customer;
+SELECT COUNT(*) FROM customer;
+
+-- Make sure to close everything 
+alter task tsk_scd_raw suspend;
